e1a63bb26159b5d970748d506a53d7285dd1b29a
Al Mooradi Dev <almooradi@gmail.com>
2023-04-04 22:40:28 +0300

    [Create Form] Bug Fix - Default value was not working

--- src/Resources/Pages/EditRecord/Concerns/Translatable.php
+++ src/Resources/Pages/EditRecord/Concerns/Translatable.php
@@ -2,9 +2,13 @@
 
 namespace Filament\Resources\Pages\EditRecord\Concerns;
 
+use Exception;
 use Filament\Resources\Pages\Concerns\HasActiveFormLocaleSwitcher;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Throwable;
 
 trait Translatable
 {
@@ -19,7 +23,6 @@ trait Translatable
         if ($this->activeFormLocale === null) {
             $this->setActiveFormLocale();
         }
-
         $data = $this->data ?? $this->record->attributesToArray();
 
         $translatableDataFromSession = session($this->getTranslatableFormDataSessionKey($this->activeFormLocale));
@@ -30,8 +33,25 @@ trait Translatable
                 $data[$attribute] = $this->record->getTranslation($attribute, $this->activeFormLocale);
             }
         }
+        // Nested translatable attributes
+        // if ($translatableDataFromSession && method_exists(static::getResource(), 'getNestedTranslatableAttributes')) {
+        //     foreach (static::getResource()::getNestedTranslatableAttributes() as $nestedTranslatableAttribute) {
+        //         $translatedData = $this->pluckAndPreserveStructure($translatableDataFromSession, $nestedTranslatableAttribute);
+        //         // $translatableAttributesData = array_merge($translatableAttributesData, $this->pluckAndPreserveStructure($data, $nestedTranslatableAttribute));
+        //         $data = array_replace_recursive($data, $translatedData);
+        //         // dd($translatedData, $data);
+        //     }
+        // }
 
         $data = $this->mutateFormDataBeforeFill($data);
+        // dd($this->form->getComponents(withHidden: true));
+        // $component = $this->getComponentByLabel('Chapters & Lessons.Chapters');
+        // $component?->mutateRelationshipDataBeforeFillUsing(function ($data) {
+        //     $data['name'] = $this->getAttributeTranslatedValue($data, 'record');
+
+        //     return $data;
+        // });
+        // dd($component);
 
         $this->form->fill($data);
 
@@ -112,8 +132,103 @@ trait Translatable
      */
     protected function saveTranslatableFormDataInSession(array $data): void
     {
-        $translatableAttributesData = Arr::only($data, $this->record->getTranslatableAttributes());
+        $resource = static::getResource();
+        $translatableAttributesData = Arr::only($data, $resource::getTranslatableAttributes());
+
+        // Nested translatable attributes
+        // if (method_exists($resource, 'getNestedTranslatableAttributes')) {
+        //     foreach ($resource::getNestedTranslatableAttributes() as $nestedTranslatableAttribute) {
+        //         $translatableAttributesData = array_merge($translatableAttributesData, $this->pluckAndPreserveStructure($data, $nestedTranslatableAttribute));
+        //     }
+        // }
+
+        // dd(session($this->getTranslatableFormDataSessionKey()));
 
         session()->put($this->getTranslatableFormDataSessionKey($this->activeFormLocale), $translatableAttributesData);
     }
+
+    protected function getAttributeTranslatedValue(array $defaultData, string $attribute, $locale = null)
+    {
+        $locale = $locale ?: $this->activeFormLocale;
+
+        $translatedData = session($this->getTranslatableFormDataSessionKey($locale));
+        
+        if (!$translatedData) {
+            return $defaultData;
+        }
+        
+        $defaultData = collect($defaultData);
+        dd($defaultData->pluck($attribute));
+    }
+
+    /**
+     * Pluck data from an array but preserve the structure
+     *
+     * @param array $array
+     * @param string $key
+     * @return mixed
+     */
+    function pluckAndPreserveStructure(array $array, string $key): mixed
+    {
+        $result = [];
+        [$firstKey, $remainingKeys] = explode('.*.', $key, 2) + [null, null];
+
+        if (is_null($remainingKeys)) {
+            // If the key does not contain `.*`, simply return the value at the key.
+            return Arr::get($array, $key, null);
+        } else {
+            // If the key contains `.*`, pluck the sub-arrays at that key and recurse.
+            foreach (Arr::get($array, $firstKey, []) as $index => $subArray) {
+                $value = $this->pluckAndPreserveStructure($subArray, $remainingKeys);
+                Arr::set($result, "{$firstKey}.{$index}.{$remainingKeys}", $value);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * TODO: Doc + fixme + warning of use (this is used to translate relationships, but we will not use anymore because we move the chapters section to a different location)
+     * ! and sometimes it enters an infinit loop
+     *
+     * @param array|string $label
+     * @param [type] $components
+     * @return void
+     */
+    // protected function getComponentByLabel(array|string $label, $components = null)
+    // {
+    //     // TODO: replace label by name
+        
+    //     $labels = is_array($label) ? $label : explode('.', $label);
+    //     $result = null;
+
+    //     $components = $components ?: $this->form->getComponents(withHidden: true);
+
+    //     $nextComponents = [];
+    //     foreach ($components as $component) {
+    //         try {
+    //             if ($component->getLabel() == $labels[0]) {
+    //                 if (count($labels) > 1) {
+    //                     $nextComponents = [$component];
+    //                     array_shift($labels);
+
+    //                     break;
+    //                 } else {
+    //                     $result = $component;
+
+    //                     break;
+    //                 }
+    //             }
+
+    //             $nextComponents = array_merge($nextComponents, $component->getChildComponents());
+    //         } catch (Throwable $th) {
+    //             // pass
+    //             // An error is thrown for some components because they are not initiated yet
+    //         }
+    //     }
+
+    //     $result = $result ?: $this->getComponentByLabel($labels, $nextComponents);
+
+    //     return $result;
+    // }
 }
